1a895517503a152cdd5d46420a10a7638a0b2bd1
Riceball LEE <snowyu.lee@gmail.com>
2015-10-06 23:17:18 +0800

    + src/index.coffee

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-require('./lib/number');
-require('./lib/string');
-require('./lib/object');
-require('./lib/function');
-require('./lib/boolean');
-module.exports = require('./lib/type-info');
+module.exports = require('./lib');
 
---
+++ src/index.coffee
@@ -0,0 +1,7 @@
+require('./number')
+require('./string')
+require('./object')
+require('./function')
+require('./boolean')
+module.exports = require('./type-info')
+
